7c0520662949abdda51479895dd160233c9046b8
Erik Nielsen <erikvn@umich.edu>
2023-11-18 18:58:35 -0500

    changes

--- Casino.cpp
+++ Casino.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <cassert>
+#include <iostream>
 
-int main() {
-    std::cout << "Hello World!";
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if(argc == 2) {
+        cout << "Please only include starting money in declaration!";
+        return -1;
+    }
+    int const startingMoney = stoi(argv[1]);
+    
+    cout << "Enter 1 for Blackjack\n";
+    cout << "Select which game you want to play: ";
+    int gamenum = -1;
+    cin >> gamenum;
     return 0;
-}
+};
+
+// ./Casino.exe 10000
